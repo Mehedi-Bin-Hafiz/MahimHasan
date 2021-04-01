b939d5a7c71292f67843a88cae59dec68aaaa892
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-04-01 18:39:30 +0600

    all loop concept

---
+++ Loop/GradingSystem.py
@@ -0,0 +1,11 @@
+
+a=80
+if(a>=80 and a<=100):
+
+    print("mahim hasan")
+
+elif(a>=70 and a<=79):
+    print("mehedi hasan")
+else:
+    print("mehedi is a bad boy")
+
---
+++ Loop/__init__.py
@@ -0,0 +1,6 @@
+
+i=1
+while i<=1:
+    print("mahim")
+    i+=1
+    
