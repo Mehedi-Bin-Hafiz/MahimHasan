63a548e3a66b377d71d38526b05e6abd6d50b021
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-08-17 07:28:46 +0600

    if else practice

--- IfElse/ifElse.py
+++ IfElse/ifElse.py
@@ -1,7 +1,7 @@
 
 a = 10
 b = 20
-if a==b:
+if a<=b:
     print('Mahim')
 else:
     print('Mehedi')
--- IfElse/practiceIfElse.py
+++ IfElse/practiceIfElse.py
@@ -1,9 +1,9 @@
 
  # two value is given, one is 10, another is 20, if their sum(+) is greater than 2nd number then show first number else show second number.
-a = 10
-b =20
+a = -12
+b = 32
 z =a+b
-print(a+b)
+
 if(z>b):
     print(10)
 else:
